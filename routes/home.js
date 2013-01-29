0abf1bb4e3d4f6b707cad915658399440f095814
Cameron Bytheway <cameron@nujii.com>
2013-01-29 00:47:08 -0700

    Make homepage pass

--- routes/home.js
+++ routes/home.js
@@ -13,6 +13,12 @@
  */
 exports.index = function() {
   return function index(req, res, next) {
-    res.send("Welcome!");
+    res.send(
+      '<h1>Welcome!</h1>'+
+      '<form method="post">'+
+        '<input name="username" type="text">'+
+        '<input name="password" type="password">'+
+      '</form>'
+    );
   };
 };
