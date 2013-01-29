20d466ccf51ab00c3f509447a471c8539ed8a1b4
Cameron Bytheway <cameron@nujii.com>
2013-01-29 00:44:15 -0700

    Define home tests

--- test/routes/home.test.js
+++ test/routes/home.test.js
@@ -1,8 +1,29 @@
-var should = require("should");
+var should = require("should")
+  , app = require("../..")
+  , request = require("supertest");
 
 describe("/", function() {
 
-  it("should load the home page");
-  it("should show a login form");
+  it("should load the home page", function(done) {
+
+    request(app)
+      .get("/")
+      .expect(/Welcome/)
+      .end(function(err) {
+        done(err);
+      });
+
+  });
+
+  it("should show a login form", function(done) {
+
+    request(app)
+      .get("/")
+      .expect(/<form/)
+      .end(function(err) {
+        done(err)
+      });
+
+  });
 
 });
